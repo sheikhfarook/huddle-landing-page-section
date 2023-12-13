1e4e47f1dbb4bd82b4034c261d5c237d41802816
Farook <mohammedfarook085@gmail.com>
2023-12-13 14:23:15 +0530

    offer components is done

---
+++ src/components/footer/offer.jsx
@@ -0,0 +1,16 @@
+const ForFree = () => {
+  return (
+    <div className="text-center space-y-10 mt-52">
+      <div className="space-y-10">
+        <h1 className="font-poppins text-4xl text-[#00252E] w-11/12 m-auto">
+          Ready To Build Your Community?
+        </h1>
+      </div>
+      <div className="font-opensans font-[600] bg-[#FF52BF] rounded-full h-14 w-1/4 m-auto ">
+        <p className="py-4 text-[#F5FAFF] ">Get Started For Free</p>
+      </div>
+    </div>
+  );
+};
+
+export default ForFree;
